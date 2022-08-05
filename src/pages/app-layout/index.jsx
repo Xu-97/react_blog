6eab9a2128c6b474e3c54c1971ed07c28b78a481
rui xu <xurui@dappworks.cn>
2022-08-05 10:52:20 +0800

    菜单配置

--- src/pages/app-layout/index.jsx
+++ src/pages/app-layout/index.jsx
@@ -1,19 +1,14 @@
-import { Layout, Menu } from 'antd'
+import { Layout } from 'antd'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import routers from '../../router'
 import "./index.less"
+import AppMenu from '../app-menu'
 const { Header, Content, Footer } = Layout
 const AppLayout = () => (
   <Layout className="layout">
     <Header>
       <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['home']}
-        items={routers[0].children}
-      />
+      <AppMenu/>
     </Header>
     <Content className="main-content">
       <Outlet/>
---
+++ src/pages/app-menu/index.jsx
@@ -0,0 +1,25 @@
+import { Menu } from 'antd'
+import React,{ useState } from 'react'
+import routers from '../../router'
+import { useNavigate } from "react-router-dom"
+
+export default function AppMenu() {
+  const navigate = useNavigate();
+  const [current, setCurrent] = useState('home');
+  const handleClick = (e) => {
+    const {key} = e
+    key === 'home' ?  navigate('/') : navigate(key)
+    setCurrent(key)
+  }
+  return (
+    <>
+    <Menu
+    theme="dark"
+    mode="horizontal"
+    selectedKeys={[current]}
+    items={routers[0].children}
+    onClick = { handleClick }
+  />
+  </>
+  )
+}
