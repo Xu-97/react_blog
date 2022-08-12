05d147caa106bd48962fb63be40ba3cecd8512dc
rui xu <xurui@dappworks.cn>
2022-08-12 22:51:05 +0800

    fix:完善博客路由组件

--- src/pages/app-menu/index.jsx
+++ src/pages/app-menu/index.jsx
@@ -1,14 +1,20 @@
 import { Menu } from 'antd'
-import React,{ useState, useEffect } from 'react'
+import React,{ useState, useEffect} from 'react'
 import routers from '../../router'
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 
 export default function AppMenu() {
   const navigate = useNavigate()
-  useEffect (() => {
-    console.log(current);
-  })
+  const loation = useLocation()
+  const { pathname } = loation 
   const [current, setCurrent] = useState('home')
+  useEffect(() =>{
+    if(pathname !== '/') {
+    const key = pathname.replace(/\//g,'')
+    setCurrent(key)
+    }
+  },[pathname])
+
   const handleClick = (e) => {
     const {key} = e
     key === 'home' ?  navigate('/') : navigate(key)
