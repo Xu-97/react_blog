ed92eb95fe3abb6d0e10b1e519bf240a3faed0ea
徐锐 <2603029264@qq.com>
2022-09-23 00:38:45 +0800

    添加我的页面，完善代码高亮

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,15 @@
 import request  from '../utils/service'
 
+// 网站日志
 export const getWebLog = () => 
   request({
     url: '/weblog',
+    method: 'GET'
+  })
+
+//关于我
+export const getAboutMe = () => 
+  request({
+    url: '/my',
     method: 'GET'
   })
--- src/pages/my/index.jsx
+++ src/pages/my/index.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { getAboutMe } from '../../api/index'
+import "./index.css"
 
 export default function MY() {
+  const [aboutMe, setAboutMe] = useState({})
+  
+  useEffect(() => {
+    const _getAboutMe = async() => {
+      const result = await getAboutMe()
+      if(result.code === 200) {
+        const {data} = result
+        setAboutMe(data[0])
+      }
+    }
+    _getAboutMe()
+  },[])
   return (
-    <div>MY</div>
+    <div className='about-me' dangerouslySetInnerHTML={{__html: aboutMe.content}}></div>
   )
 }
