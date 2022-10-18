1517be122c53aa0d5e8b0d83b798f61279ec900c
徐锐 <2603029264@qq.com>
2022-10-19 00:22:53 +0800

    样式调优

--- src/pages/bottom/index.js
+++ src/pages/bottom/index.js
@@ -5,8 +5,9 @@ export default function Footer() {
     window.open('https://beian.miit.gov.cn/')
   }
   return (
-    <div style={{fontSize:'12px',color:'#AAA'} }onClick={handleClick}>
-      <p><img src='https://cdn.heblogs.cn/beian.png' alt='' />皖ICP备2021012350号-1</p>
+    <div style={{fontSize:'12px',color:'#AAA'} }>
+      <p>Copyright © 2021 劲夫随笔</p>
+      <p onClick={handleClick}><img src='https://cdn.heblogs.cn/beian.png' alt='' />皖ICP备2021012350号-1</p>
     </div>
   )
 }
