5f9dac46c7d134779c2b6bd8cd658348fa8e2c05
徐锐 <2603029264@qq.com>
2022-11-02 23:11:58 +0800

    兼容苹果，使用Moment.js

--- src/utils/help.js
+++ src/utils/help.js
@@ -1,41 +1,46 @@
 // 日期格式化
-export const parseTime =  function(time, pattern) {
-  if (arguments.length === 0 || !time) {
-    return null
-  }
-  const format = pattern || '{yy}/{m}/{d} {h}:{i}:{s}'
-  let date
-  if (typeof time === 'object') {
-    date = time
-  } else {
-    if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
-    } else if (typeof time === 'string') {
-      time = time.replace(new RegExp(/-/gm), "/").replace('T', ' ').replace(new RegExp(/\.[\d]{3}/gm), '');
-    }
-    if ((typeof time === 'number') && (time.toString().length === 10)) {
-      time = time * 1000
-    }
-    date = new Date(time)
-  }
-  const formatObj = {
-    y: date.getFullYear(),
-    m: date.getMonth() + 1,
-    d: date.getDate(),
-    h: date.getHours(),
-    i: date.getMinutes(),
-    s: date.getSeconds(),
-    a: date.getDay()
-  }
-  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
-    let value = formatObj[key]
-    // Note: getDay() returns 0 on Sunday
-    if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value] }
-    if (result.length > 0 && value < 10) {
-      value = '0' + value
-    }
-    return value || 0
-  })
-  return time_str
+// export const parseTime =  function(time, pattern) {
+//   if (arguments.length === 0 || !time) {
+//     return null
+//   }
+//   const format = pattern || '{yy}/{m}/{d} {h}:{i}:{s}'
+//   let date
+//   if (typeof time === 'object') {
+//     date = time
+//   } else {
+//     if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+//     } else if (typeof time === 'string') {
+//       time = time.replace(new RegExp(/-/gm), "/").replace('T', ' ').replace(new RegExp(/\.[\d]{3}/gm), '');
+//     }
+//     if ((typeof time === 'number') && (time.toString().length === 10)) {
+//       time = time * 1000
+//     }
+//     date = new Date(time)
+//   }
+//   const formatObj = {
+//     y: date.getFullYear(),
+//     m: date.getMonth() + 1,
+//     d: date.getDate(),
+//     h: date.getHours(),
+//     i: date.getMinutes(),
+//     s: date.getSeconds(),
+//     a: date.getDay()
+//   }
+//   const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+//     let value = formatObj[key]
+//     // Note: getDay() returns 0 on Sunday
+//     if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value] }
+//     if (result.length > 0 && value < 10) {
+//       value = '0' + value
+//     }
+//     return value || 0
+//   })
+//   return time_str
+// }
+import moment from "moment";
+
+export const parseTime = function(time,pattern='YYYY-MM-DD-hh:mm:ss'){
+  return moment(time).format(pattern)
 }
 
 
