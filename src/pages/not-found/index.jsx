12ce9524ee4940dfbd632328f1c90ccdef8f672c
徐锐 <2603029264@qq.com>
2023-01-14 21:38:03 +0800

    添加404页面

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Empty } from 'antd';
+import src from '@/assets/images/404.svg'
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <Empty
+        style={{
+          height: '100vh',
+          display: 'flex', 
+          flexDirection: 'column',
+          justifyContent: ' center',
+          alignItems: 'center'
+        }}
+        image={src}
+        description="你访问的资源不存在"
+        imageStyle={{ minHeight: 200, width: '100%' }} />
+    </>
   )
 }
